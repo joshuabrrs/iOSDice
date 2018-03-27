3022963022583fa4382ac2af99338bcf4e943097
joshuabrrs <jgb16@my.fsu.edu>
2018-03-27 16:07:21 -0400

    the app seems to be crashing...idk

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -1,26 +1,39 @@
-//
 //  ViewController.swift
 //  Dice
-//
 //  Created by Joshua Barrios on 3/27/18.
 //  Copyright © 2018 Joshua Barrios. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    
+    let diceArray = ["dice1","dice2","dice3","dice4","dice5","dice6"]
+    
+    //IBOutlet means that the appearance is changed
     @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  
     }
-
-
+    // IBAction means that the UI element is interacted with
+    @IBAction func rollBttnPressed(_ sender: UIButton) {
+        //random generator... arc4random_uniform
+        //type catsing into int
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        //diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+    }
+    
 }
 
