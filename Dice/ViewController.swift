391ae2a419713d7e8cf23ad2b1fc34b4e2eba419
joshuabrrs <jgb16@my.fsu.edu>
2018-03-27 14:26:50 -0400

    First Commit

---
+++ Dice/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Dice
+//
+//  Created by Joshua Barrios on 3/27/18.
+//  Copyright © 2018 Joshua Barrios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
