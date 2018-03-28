84e8516b5a37172cc840f725173d36c9380a139f
joshuabrrs <jgb16@my.fsu.edu>
2018-03-27 22:26:52 -0400

    added a few more functions, used motion detection in the app, still have to implement auto layout

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -6,18 +6,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     var randomDiceIndex1 : Int = 0
     var randomDiceIndex2 : Int = 0
+    
+    //this array will manipulate the images
     
     let diceArray = ["dice1","dice2","dice3","dice4","dice5","dice6"]
     
     //IBOutlet means that the appearance is changed
+    
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        updateDiceImages()
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,15 +29,27 @@ class ViewController: UIViewController {
   
     }
     // IBAction means that the UI element is interacted with
+    
     @IBAction func rollBttnPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages() {
         //random generator... arc4random_uniform
         //type catsing into int
+        
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
         
+        //this is where we can see the dice being randomized
+        //assigned the random number to array and changed the image
+        
         diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
-        //diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+        
     }
-    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
 }
 
